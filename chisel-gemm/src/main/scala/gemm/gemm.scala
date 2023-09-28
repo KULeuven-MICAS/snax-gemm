08ec639c0e2d2fe6a60b1858138f0760c0d036d5
xiaoling-yi <xiaoling.yi@student.kuleuven.be>
2023-09-28 13:56:37 +0200

    commnets on gemm

--- chisel-gemm/src/main/scala/gemm/gemm.scala
+++ chisel-gemm/src/main/scala/gemm/gemm.scala
@@ -1,40 +1,37 @@
 package gemm
 import chisel3._
 import chisel3.util._
-import chisel3.experimental._
-// import chisel3.experimental.{chiselName,NoChiselNamePrefix}
-import chisel3.experimental.IntParam
 import chisel3.VecInit
-import chisel3.util.HasBlackBoxResource
-import scala.util.matching.Regex
-import chisel3.util.MixedVec
 
+// Control signals for Gemm. Cuurently, we only have data_in_valid
 class TileControl extends Bundle{
-    val data_in_valid = Input(Bool())
+    val data_in_valid = Input(Bool())    
 }
 
+// Tile IO definition
 class TileIO extends Bundle{
-    val a_io_in = Input(Vec(GEMMConstant.meshRow,GEMMConstant.inputType))
-    val b_io_in = Input(Vec(GEMMConstant.meshCol,GEMMConstant.inputType))
-    val c_io_out = Output(GEMMConstant.outputType)
-    val data_out_valid = Output(Bool())   
-
+    val a_io_in = Input(Vec(GEMMConstant.tileSize,UInt(GEMMConstant.input.W)))
+    val b_io_in = Input(Vec(GEMMConstant.tileSize,UInt(GEMMConstant.input.W)))
+    val c_io_out = Output(UInt(GEMMConstant.output.W))
     val in_control = Input(new TileControl())
+    val data_out_valid = Output(Bool())  
 }
 
+// Tile implementation, do a vector dot product of two vector
+// When data_in_valid assert, do the computation, and give the result next cycle, with a data_out_valid assert
 class Tile extends Module{
     val io = IO (new TileIO())
  
     val accumulation_reg = RegInit(0.U(GEMMConstant.acc.W))  
     val chek_data_in_valid = RegInit(0.B)
     val mul_add_result = Wire(UInt(GEMMConstant.mul.W))
-    val mul_add_result_vec_reg = Wire(Vec(GEMMConstant.meshRow,GEMMConstant.accType))
+    val mul_add_result_vec_reg = Wire(Vec(GEMMConstant.tileSize,UInt(GEMMConstant.mul.W)))
 
     chisel3.dontTouch(mul_add_result)
     
     chek_data_in_valid := io.in_control.data_in_valid
 
-    for( i <- 0 until GEMMConstant.meshRow){
+    for( i <- 0 until GEMMConstant.tileSize){
         mul_add_result_vec_reg(i) := io.a_io_in(i) * io.b_io_in(i)
     }  
 
@@ -51,14 +48,16 @@ class Tile extends Module{
     io.data_out_valid := chek_data_in_valid
 }
 
+// Mesh IO definition, an extended version of Tile IO
 class MeshIO extends Bundle{
-    val a_io_in = Input(Vec(GEMMConstant.meshRow,Vec(GEMMConstant.meshCol, GEMMConstant.inputType)))
-    val b_io_in = Input(Vec(GEMMConstant.meshCol,Vec(GEMMConstant.meshRow,GEMMConstant.inputType)))
-    val c_io_out = Output((Vec(GEMMConstant.meshRow,Vec(GEMMConstant.meshCol,GEMMConstant.outputType))))
+    val a_io_in = Input(Vec(GEMMConstant.meshRow,Vec(GEMMConstant.tileSize, UInt(GEMMConstant.input.W))))
+    val b_io_in = Input(Vec(GEMMConstant.meshCol,Vec(GEMMConstant.tileSize,UInt(GEMMConstant.input.W))))
+    val c_io_out = Output((Vec(GEMMConstant.meshRow,Vec(GEMMConstant.meshCol,UInt(GEMMConstant.output.W)))))
     val in_control = Input(new TileControl())
-    val data_out_valid = Output(Bool())
+    val data_out_valid = Output(Bool())  
 }
 
+// Mesh implementation, just creat a mesh of TIles and do the connection
 class Mesh extends Module{
 
     val io = IO(new MeshIO())
@@ -78,15 +77,16 @@ class Mesh extends Module{
     io.data_out_valid := mesh(0)(0).io.data_out_valid
 }
 
+// Gemm IO definition
 class  GemmIO extends Bundle{
     val data_in_valid = Input(Bool())
-    val a_io_in = Input(UInt((GEMMConstant.meshRow * GEMMConstant.meshCol * GEMMConstant.inputLen).W))
-    val b_io_in = Input(UInt((GEMMConstant.meshRow * GEMMConstant.meshCol * GEMMConstant.inputLen).W))
-
+    val a_io_in = Input(UInt((GEMMConstant.meshRow * GEMMConstant.tileSize * GEMMConstant.input).W))
+    val b_io_in = Input(UInt((GEMMConstant.tileSize * GEMMConstant.meshCol * GEMMConstant.input).W))
     val data_out_valid = Output(Bool())
-    val c_io_out = Output(UInt((GEMMConstant.meshRow * GEMMConstant.meshCol * GEMMConstant.outputLen).W))
+    val c_io_out = Output(UInt((GEMMConstant.meshRow * GEMMConstant.meshCol * GEMMConstant.output).W))
 }
 
+// Gemm implementation, create a Mesh and give out input data and collect results of each Tile
 class Gemm extends Module{
 
     val io = IO(new GemmIO())
@@ -94,15 +94,25 @@ class Gemm extends Module{
     val mesh = Module(new Mesh())
 
     // data partition
-    val a_in_wire = Wire(Vec(GEMMConstant.meshRow,Vec(GEMMConstant.meshCol, GEMMConstant.inputType)))
-    val b_in_wire = Wire(Vec(GEMMConstant.meshCol,Vec(GEMMConstant.meshRow, GEMMConstant.inputType)))
-    val c_out_wire = Wire(Vec(GEMMConstant.meshRow,Vec(GEMMConstant.meshCol, GEMMConstant.outputType)))
-    val c_out_wire_2 = Wire(Vec(GEMMConstant.meshRow,UInt((GEMMConstant.meshCol * GEMMConstant.outputLen).W)))
+    val a_in_wire = Wire(Vec(GEMMConstant.meshRow,Vec(GEMMConstant.tileSize, UInt(GEMMConstant.input.W))))
+    val b_in_wire = Wire(Vec(GEMMConstant.meshCol,Vec(GEMMConstant.tileSize, UInt(GEMMConstant.input.W))))
+    val c_out_wire = Wire(Vec(GEMMConstant.meshRow,Vec(GEMMConstant.meshCol, UInt(GEMMConstant.input.W))))
+    val c_out_wire_2 = Wire(Vec(GEMMConstant.meshRow,UInt((GEMMConstant.meshCol * GEMMConstant.output).W)))
+
+    for( r <- 0 until GEMMConstant.meshRow){
+        for ( c <- 0 until GEMMConstant.tileSize){
+            a_in_wire(r)(c) := io.a_io_in((r * GEMMConstant.tileSize  + c  + 1) * GEMMConstant.input - 1, (r * GEMMConstant.tileSize  + c ) * GEMMConstant.input) 
+        }
+    }    
+
+    for( r <- 0 until GEMMConstant.meshCol){
+        for ( c <- 0 until GEMMConstant.tileSize){
+            b_in_wire(r)(c) := io.b_io_in((r * GEMMConstant.tileSize  + c  + 1) * GEMMConstant.input - 1, (r * GEMMConstant.tileSize  + c ) * GEMMConstant.input)
+        }
+    }
 
     for( r <- 0 until GEMMConstant.meshRow){
         for ( c <- 0 until GEMMConstant.meshCol){
-            a_in_wire(r)(c) := io.a_io_in((r * GEMMConstant.meshRow  + c  + 1) * GEMMConstant.inputLen - 1, (r * GEMMConstant.meshRow  + c ) * GEMMConstant.inputLen) 
-            b_in_wire(r)(c) := io.b_io_in((r * GEMMConstant.meshCol  + c  + 1) * GEMMConstant.inputLen - 1, (r * GEMMConstant.meshCol  + c ) * GEMMConstant.inputLen)
             c_out_wire(r)(c) := mesh.io.c_io_out(r)(c)            
         }
         c_out_wire_2(r) := Cat(c_out_wire(r).reverse)
@@ -111,8 +121,8 @@ class Gemm extends Module{
     // data signal connect
     a_in_wire <> mesh.io.a_io_in
     b_in_wire <> mesh.io.b_io_in
-
     io.c_io_out := Cat(c_out_wire_2.reverse)
-    mesh.io.in_control.data_in_valid := io.data_in_valid
+
+    mesh.io.in_control.data_in_valid <> io.data_in_valid
     io.data_out_valid := mesh.io.data_out_valid
 }
--- chisel-gemm/src/main/scala/gemm/parameters.scala
+++ chisel-gemm/src/main/scala/gemm/parameters.scala
@@ -3,31 +3,13 @@ import chisel3._
 import chisel3.util._
 
 object GEMMConstant{
-    def W_DATA = 32
-    def W_ADDR = 32
+
     def input = 8 
-    def inputType = UInt(input.W)
-    def inputLen = input
-    def inputLenUInt = input.U 
-
-    def mul = 8 * 2
-    def mulType = UInt(mul.W) 
-    def mulLenUInt = mul.U 
-    def mulLen = mul
-
-    def output = 8 * 4
-    def outputType = UInt(output.W) 
-    def outputLenUInt = output.U 
-    def outputLen = output
-
-    def acc = 8 * 4
-    def accType = UInt(acc.W) 
-    def accLen = acc
-    
-    def dim = 8
-    def meshRow = dim
-    def meshCol = dim
-    def meshRowUInt = dim.U
-    def meshColUInt = dim.U
+    def mul = input * 2
+    def output = input * 4
+    def acc = input * 4
+    def tileSize = 4
+    def meshRow = 8
+    def meshCol = 6
 
 }
