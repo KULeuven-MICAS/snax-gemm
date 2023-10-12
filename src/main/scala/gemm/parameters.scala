879c9039a0db67132fe574c96dab7d7aa191a877
xiaoling-yi <xiaoling.yi@student.kuleuven.be>
2023-10-12 16:32:56 +0200

    sw: Add scalafmt configuration file

--- src/main/scala/gemm/parameters.scala
+++ src/main/scala/gemm/parameters.scala
@@ -2,14 +2,14 @@ package gemm
 import chisel3._
 import chisel3.util._
 
-object GEMMConstant{
+object GEMMConstant {
 
-    def input = 8 
-    def mul = input * 2
-    def output = input * 4
-    def acc = input * 4
-    def tileSize = 8
-    def meshRow = 8
-    def meshCol = 8
+  def input = 8
+  def mul = input * 2
+  def output = input * 4
+  def acc = input * 4
+  def tileSize = 8
+  def meshRow = 8
+  def meshCol = 8
 
 }
