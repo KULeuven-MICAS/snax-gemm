f0f46debe998983b953059f73af170df7482b296
xiaoling-yi <xiaoling.yi@student.kuleuven.be>
2023-10-16 20:26:42 +0200

    hw: mul paremater

--- src/main/scala/gemm/parameters.scala
+++ src/main/scala/gemm/parameters.scala
@@ -5,7 +5,7 @@ import chisel3.util._
 object GEMMConstant {
 
   def input = 8
-  def mul = input * 2
+  def mul = input * 4
   def output = input * 4
   def accumulate = input * 4
   def tileSize = 8
