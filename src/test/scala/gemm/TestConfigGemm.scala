b6d48d1019de48d496665ebbb0aabd9abc46abf3
xiaoling-yi <xiaoling.yi@student.kuleuven.be>
2023-11-30 10:21:54 +0100

    hw: add tets for 4 type of gemm

--- src/test/scala/gemm/TestConfigGemm.scala
+++ src/test/scala/gemm/TestConfigGemm.scala
@@ -3,15 +3,24 @@ import chisel3._
 import org.scalatest.flatspec.AnyFlatSpec
 import chiseltest._
 
+// Test for GEMM Generator
 class ConfigGemmTest extends AnyFlatSpec with ChiselScalatestTester {
   "DUT" should "pass" in {
-    def GemmType = 4
-    test(new GemmGenerator)
-      .withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
-        emitVerilog(
-          new GemmGenerator(GemmType),
-          Array("--target-dir", "generated/gemm")
-        )
-      }
+
+    def GenGEMM(GemmType: Int) = {
+      test(new GemmGenerator)
+        .withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
+          emitVerilog(
+            new GemmGenerator(GemmType),
+            Array("--target-dir", "generated/gemm")
+          )
+        }
+    }
+
+    GenGEMM(1)
+    GenGEMM(2)
+    GenGEMM(3)
+    GenGEMM(4)
+
   }
 }
