b8fc97f188faa087f58cc94c43150f9bcadfeafd
Xiaoling Yi <143962462+xiaoling-yi@users.noreply.github.com>
2024-03-08 09:25:14 +0100

    Modify for integrating with streamer (#43)
    * modify baregemm for streamer integeration
    
    * fix output valid bug
    
    * modify comment for N and K
    
    * modify bender target
    
    * data == 1 start

--- src/main/scala/gemm/CsrManager.scala
+++ src/main/scala/gemm/CsrManager.scala
@@ -123,7 +123,7 @@ class GemmCsrManager(
   io.csr_config_in.req.ready := (io.csr_config_out.ready || !(io.csr_config_in.req.bits.addr === startCsrAddr)) && (!keep_sending_csr_rsp || io.csr_config_in.req.bits.write)
 
   // a write/read to the last csr means the config is valid
-  config_valid := io.csr_config_in.req.fire && (io.csr_config_in.req.bits.addr === startCsrAddr)
+  config_valid := io.csr_config_in.req.fire && (io.csr_config_in.req.bits.addr === startCsrAddr) && io.csr_config_in.req.bits.data === 1.U
 
   // signals connected to the output ports
   io.csr_config_out.bits <> csr
